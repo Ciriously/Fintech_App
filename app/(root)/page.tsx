f54a720f2e502e02be7ce6e1a9e661531239c9af
Aditya Mishra <56506651+Ciriously@users.noreply.github.com>
2024-06-09 13:08:52 +0530

    Feat : implemented Chart-js

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,29 @@
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import HeaderBox from "@/components/HeaderBox";
 import React from "react";
 
-const page = () => {
-  return <div>page</div>;
+const Home = () => {
+  const loggedIn = { firstName: "ADI" };
+
+  return (
+    <section className="home ">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Welcome to Horizon, your modern banking platform."
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={30000}
+          />
+        </header>
+      </div>
+    </section>
+  );
 };
 
-export default page;
+export default Home;
